291bdc832d1fdd364b8b7734503f49775e0b1755
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-01 16:43:30 +0100

    Create one function responsible for scraping

--- src/lib.rs
+++ src/lib.rs
@@ -124,152 +124,128 @@ pub mod job_offers_scraper {
         }
     }
 
-    pub fn scrap_justjoinit_job_offers(tech: &String) -> Vec<JobOffer> {
-        let mut job_offers: Vec<JobOffer> = Vec::new();
-
-        let response = match tech.as_str() {
-            "java" | "python" => reqwest::blocking::get(format!(
-                "https://justjoin.it/job-offers/all-locations/{tech}?experience-level=junior"
-            )),
-            _ => reqwest::blocking::get(
-                "https://justjoin.it/job-offers/all-locations/java?experience-level=junior",
-            ),
-        };
-        let html_content = response.unwrap().text().unwrap();
-        let document = scraper::Html::parse_document(&html_content);
-
-        let html_job_offers_selector =
-            scraper::Selector::parse("div.MuiBox-root.css-8xzgzu").unwrap();
-        let html_job_offers = document.select(&html_job_offers_selector);
-
-        for html_job_offer in html_job_offers {
-            let title = html_job_offer
-                .select(&scraper::Selector::parse("h3.css-3hs82j").unwrap())
-                .next()
-                .map(|h3| h3.text().collect::<String>());
-            let company = html_job_offer
-                .select(&scraper::Selector::parse("span").unwrap())
-                .next()
-                .map(|span| span.text().collect::<String>());
-            let location = html_job_offer
-                .select(&scraper::Selector::parse("span.css-1o4wo1x").unwrap())
-                .next()
-                .map(|span| span.text().collect::<String>());
-            let href = html_job_offer
-                .select(&scraper::Selector::parse("a").unwrap())
-                .next()
-                .and_then(|a| a.value().attr("href"))
-                .map(str::to_owned);
-            let mut full_href = href.clone().unwrap().to_owned();
-            full_href.insert_str(0, "https://justjoin.it");
-            let link = Some(full_href);
-
-            let job_offer = JobOffer {
-                title,
-                company,
-                location,
-                link,
-                source: Some(Source::JustJoinIt),
-            };
-
-            job_offers.push(job_offer);
-        }
-
-        return job_offers;
+    fn create_link_with_prefix(prefix: &str, href: String) -> String {
+        let mut link = href;
+        link.insert_str(0, &prefix);
+        link
     }
 
-    pub fn scrap_nofluffjobs_job_offers(tech: &String) -> Vec<JobOffer> {
+    pub fn scrap_job_offers(source: Source, tech: &String) -> Vec<JobOffer> {
         let mut job_offers: Vec<JobOffer> = Vec::new();
 
-        let response = match tech.as_str() {
-            "java" | "python" => reqwest::blocking::get(format!(
-                "https://nofluffjobs.com/pl/{tech}?criteria=seniority%3Dtrainee%2Cjunior"
-            )),
-            _ => reqwest::blocking::get(
-                "https://nofluffjobs.com/pl/java?criteria=seniority%3Dtrainee%2Cjunior",
-            ),
+        let url: String = match source {
+            Source::NotProvided => panic!("Invalid source"),
+            Source::JustJoinIt => {
+                if tech.eq("java") || tech.eq("python") {
+                    format!("https://justjoin.it/job-offers/all-locations/{tech}?experience-level=junior")
+                } else {
+                    "https://justjoin.it/job-offers/all-locations/java?experience-level=junior"
+                        .to_string()
+                }
+            }
+            Source::NoFluffJobs => {
+                if tech.eq("java") || tech.eq("python") {
+                    format!(
+                        "https://nofluffjobs.com/pl/{tech}?criteria=seniority%3Dtrainee%2Cjunior"
+                    )
+                } else {
+                    "https://nofluffjobs.com/pl/java?criteria=seniority%3Dtrainee%2Cjunior"
+                        .to_string()
+                }
+            }
+            Source::PracujPl => {
+                if tech.eq("java") {
+                    "https://it.pracuj.pl/praca?et=1%2C3%2C17&itth=38".to_string()
+                } else if tech.eq("python") {
+                    "https://it.pracuj.pl/praca?et=1%2C3%2C17&itth=37".to_string()
+                } else {
+                    "https://it.pracuj.pl/praca?et=1%2C3%2C17&itth=38".to_string()
+                }
+            }
         };
+        let response = reqwest::blocking::get(url);
+
         let html_content = response.unwrap().text().unwrap();
         let document = scraper::Html::parse_document(&html_content);
 
-        let html_job_offers_selector = scraper::Selector::parse("a.posting-list-item").unwrap();
+        let html_job_offers_selector = match source {
+            Source::NotProvided => panic!("Invalid source"),
+            Source::JustJoinIt => scraper::Selector::parse("div.MuiBox-root.css-8xzgzu").unwrap(),
+            Source::NoFluffJobs => scraper::Selector::parse("a.posting-list-item").unwrap(),
+            Source::PracujPl => scraper::Selector::parse("div.tiles_cobg3mp").unwrap(),
+        };
         let html_job_offers = document.select(&html_job_offers_selector);
 
         for html_job_offer in html_job_offers {
+            let title_selector = match source {
+                Source::NotProvided => panic!("Invalid source"),
+                Source::JustJoinIt => "h3.css-3hs82j",
+                Source::NoFluffJobs => "h3.posting-title__position",
+                Source::PracujPl => "h2.tiles_h1p4o5k6",
+            };
             let title = html_job_offer
-                .select(&scraper::Selector::parse("h3.posting-title__position").unwrap())
+                .select(&scraper::Selector::parse(&title_selector).unwrap())
                 .next()
-                .map(|h3| h3.text().collect::<String>());
-            let company = html_job_offer
-                .select(&scraper::Selector::parse("h4.company-name").unwrap())
-                .next()
-                .map(|h4| h4.text().collect::<String>());
-            let location = html_job_offer
-                .select(&scraper::Selector::parse("span.tw-text-right").unwrap())
-                .next()
-                .map(|span| span.text().collect::<String>());
-            let href = html_job_offer.value().attr("href");
-            let mut full_href = href.clone().unwrap().to_owned();
-            full_href.insert_str(0, "https://nofluffjobs.com");
-            let link = Some(full_href);
+                .map(|e| e.text().collect::<String>());
 
-            let job_offer = JobOffer {
-                title,
-                company,
-                location,
-                link,
-                source: Some(Source::NoFluffJobs),
+            let company_selector = match source {
+                Source::NotProvided => panic!("Invalid source"),
+                Source::JustJoinIt => "span",
+                Source::NoFluffJobs => "h4.company-name",
+                Source::PracujPl => "h3.tiles_chl8gsf.size-caption.core_t1rst47b",
+            };
+            let company = html_job_offer
+                .select(&scraper::Selector::parse(&company_selector).unwrap())
+                .next()
+                .map(|e| e.text().collect::<String>());
+
+            let location_selector = match source {
+                Source::NotProvided => panic!("Invalid source"),
+                Source::JustJoinIt => "span.css-1o4wo1x",
+                Source::NoFluffJobs => "span.tw-text-right",
+                Source::PracujPl => "h4.size-caption.core_t1rst47b",
+            };
+            let location = html_job_offer
+                .select(&scraper::Selector::parse(&location_selector).unwrap())
+                .next()
+                .map(|e| e.text().collect::<String>());
+
+            let link = match source {
+                Source::NotProvided => panic!("Invalid source"),
+                Source::JustJoinIt => {
+                    let href = html_job_offer
+                        .select(&scraper::Selector::parse("a").unwrap())
+                        .next()
+                        .and_then(|a| a.value().attr("href"))
+                        .map(str::to_owned);
+                    Some(create_link_with_prefix(
+                        "https://justjoin.it",
+                        href.unwrap(),
+                    ))
+                }
+                Source::NoFluffJobs => {
+                    let href = html_job_offer.value().attr("href").map(str::to_owned);
+                    Some(create_link_with_prefix(
+                        "https://nofluffjobs.com",
+                        href.unwrap(),
+                    ))
+                }
+                Source::PracujPl => {
+                    let href = html_job_offer
+                        .select(&scraper::Selector::parse("a.core_n194fgoq").unwrap())
+                        .next()
+                        .and_then(|e| e.value().attr("href"))
+                        .map(str::to_owned);
+                    Some(href.unwrap())
+                }
             };
 
-            job_offers.push(job_offer);
-        }
-
-        return job_offers;
-    }
-
-    pub fn scrap_pracujpl_job_offers(tech: &String) -> Vec<JobOffer> {
-        let mut job_offers: Vec<JobOffer> = Vec::new();
-
-        let response = match tech.as_str() {
-            "java" => reqwest::blocking::get("https://it.pracuj.pl/praca?et=1%2C3%2C17&itth=38"),
-            "python" => reqwest::blocking::get("https://it.pracuj.pl/praca?et=1%2C3%2C17&itth=37"),
-            _ => reqwest::blocking::get("https://it.pracuj.pl/praca?et=1%2C3%2C17&itth=38"),
-        };
-        let html_content = response.unwrap().text().unwrap();
-        let document = scraper::Html::parse_document(&html_content);
-
-        let html_job_offers_selector = scraper::Selector::parse("div.tiles_cobg3mp").unwrap();
-        let html_job_offers = document.select(&html_job_offers_selector);
-
-        for html_job_offer in html_job_offers {
-            let title = html_job_offer
-                .select(&scraper::Selector::parse("h2.tiles_h1p4o5k6").unwrap())
-                .next()
-                .map(|h3| h3.text().collect::<String>());
-            let company = html_job_offer
-                .select(
-                    &scraper::Selector::parse("h3.tiles_chl8gsf.size-caption.core_t1rst47b")
-                        .unwrap(),
-                )
-                .next()
-                .map(|h3| h3.text().collect::<String>());
-            let location = html_job_offer
-                .select(&scraper::Selector::parse("h4.size-caption.core_t1rst47b").unwrap())
-                .next()
-                .map(|h4| h4.text().collect::<String>());
-            let full_href = html_job_offer
-                .select(&scraper::Selector::parse("a.core_n194fgoq").unwrap())
-                .next()
-                .and_then(|a| a.value().attr("href"))
-                .map(str::to_owned);
-            let link = Some(full_href.unwrap());
-
             let job_offer = JobOffer {
                 title,
                 company,
                 location,
                 link,
-                source: Some(Source::PracujPl),
+                source: Some(source.clone()),
             };
 
             job_offers.push(job_offer);
